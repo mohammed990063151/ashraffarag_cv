0c2df604e509c547c3c71fe4fd5541d0d2830f8d
mohammed mustafa <mohammed990063151@gmail.com>
2025-12-21 10:40:25 +0300

    update in img agin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -26,14 +26,21 @@ License URL: https://creativecommons.org/licenses/by/4.0/
     <link href="{{ asset('../css/simple-line-icons/css/simple-line-icons.css') }}" rel="stylesheet">
     <link href="{{ asset('../css/style.css') }}" rel="stylesheet">
 
-    <style>
-        #about {
-    background-image: url({{ $profile ? asset('storage/app/public/'.$profile->profile_image) : asset('img/profile.jpg') }});
+  @php
+    $bgImage = $profile && $profile->profile_image 
+        ? Storage::url($profile->profile_image) 
+        : asset('img/profile.jpg');
+@endphp
+
+<style>
+#about {
+    background-image: url({{ $bgImage }});
     background-size: cover;
     background-position: center center;
     background-repeat: no-repeat;
 }
-    </style>
+</style>
+
 
     
 </head>
